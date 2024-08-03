4930eb5706a6fba1ef93fb40dc11994fd88e8afe
ravi <sama369.rk@gmail.com>
2024-08-03 10:00:43 +0530

    learning php

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Learning PHP</title>
+</head>
+<body>
+
+          <!-- Hypertext pre-proccessor -->
+           <!-- it's a server side scripting lang -->
+
+       <!-- MVC - model , view , controller   -  view- static web, controller-php, model-database -->
+
+
+
+
+
+       <!-- php -->
+        <!-- simple and easy to learn -->
+         <!-- open source  and freeware -->
+          <!-- object oriented -->
+           <!-- platform independed -->
+            <!-- vast community support -->
+
+
+
+
+
+            
+            
+            
+    <?php
+    
+    
+    
+    ?>
+
+
+
+
+
+
+
+
+<!-- <div class='container'>
+    this is my first php 
+</div> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    // echo 'hello';
+
+    // $fvar = "this is variable\n";
+    // echo $fvar;
+    
+
+    // $val1 ="i am\t";
+    // $val2 ="Ravi";
+
+    // echo $val1.$val2 ."\tweb dev";
+    
+//    $age = 18;
+
+//    if($age>= 18){
+//     echo "i am adult";
+//    }
+//    else{
+//      echo "fucking kid";
+//    }
+
+
+    ?>
+</body>
+</html>
